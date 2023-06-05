46289e58cfd73d779d8cecff9f073ea8c9fd971d
Gello <IWorking4life101@gmail.com>
2023-06-04 21:36:24 -0400

    I've setup the devil hopefully the next step is ok

---
+++ yumyum/src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2be5rZ9bkRKnnrmniCKceW7hASSR3u3Y",
+  authDomain: "summativegrade12-eeb24.firebaseapp.com",
+  projectId: "summativegrade12-eeb24",
+  storageBucket: "summativegrade12-eeb24.appspot.com",
+  messagingSenderId: "716986285888",
+  appId: "1:716986285888:web:c5a51f4ab2c8a7226584a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
